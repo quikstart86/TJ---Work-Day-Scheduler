32d6287f142020248f8404f9c3d00dcf6120c1ad
Thomas <123500706+quikstart86@users.noreply.github.com>
2024-01-05 21:52:25 +0000

    Browser not saving the text - made  edits to the code to figure out why its not retrieving it on refresh

--- assets/script.js
+++ assets/script.js
@@ -31,26 +31,20 @@ function updateHour() {
 // Calling the function to update the hours
 updateHour();
 
-$(`.saveBtn`).on(`click`, function (){
-    let textValue = $(this).siblings(`.description`).val();
-
-    let timeValue = $(this).parent().attr(`id`);
-
+$('.saveBtn').on('click', function() {
+    let textValue = $(this).siblings('.description').val();
+    let timeValue = $(this).parent().attr('id');
     localStorage.setItem(timeValue, textValue);
+    console.log(`Saved: ${timeValue} - ${textValue}`);
 });
-// to get item from local storage
-$('#8 .description').val(localStorage.getItem('8'));
-$('#9 .description').val(localStorage.getItem('9'));
-$('#10 .description').val(localStorage.getItem('10'));
-$('#11 .description').val(localStorage.getItem('11'));
-$('#12 .description').val(localStorage.getItem('12'));
-$('#13 .description').val(localStorage.getItem('13'));
-$('#14 .description').val(localStorage.getItem('14'));
-$('#15 .description').val(localStorage.getItem('15'));
-$('#16 .description').val(localStorage.getItem('16'));
-$('#17 .description').val(localStorage.getItem('17'));
-$('#18 .description').val(localStorage.getItem('18'));
+
+$('.time-block').each(function() {
+    let hour = $(this).attr('id');
+    let storedValue = localStorage.getItem(hour);
+    $(this).find('.description').val(storedValue);
+    console.log(`Retrieved: ${hour} - ${storedValue}`);
+});
+});
 
 
-
-})
+^^ Confused ^^
