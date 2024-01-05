db1599c449c656079bfcb56dbc8ad4f1a3a64c88
Thomas <123500706+quikstart86@users.noreply.github.com>
2024-01-05 21:31:32 +0000

    added in JS code to retrieve items from local storage

--- assets/script.js
+++ assets/script.js
@@ -37,7 +37,19 @@ $(`.saveBtn`).on(`click`, function (){
     let timeValue = $(this).parent().attr(`id`);
 
     localStorage.setItem(timeValue, textValue);
-})
+});
+// to get item from local storage
+$('#8 .description').val(localStorage.getItem('8'));
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
+$('#18 .description').val(localStorage.getItem('18'));
 
 
 
