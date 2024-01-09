635670d881963dedcfd53f44717276f88246b794
Thomas <123500706+quikstart86@users.noreply.github.com>
2024-01-09 22:48:43 +0000

    finshied of the last bit of styling and added in comments to all the HTML/CSS/JS code. Tested the app in browser

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,58 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    // dayjs plugin for date and time function on app displaying day/month/year + hours and mins (am/pm)
     function displayTime() {
-        $(`#currentDay`).text(dayjs().format (`dddd, MMMM D, YYYY`))
+        $(`#currentDay`).text(dayjs().format(`dddd, MMMM D, YYYY`))
         $(`#currentTime`).text(dayjs().format(`HH:mm A`))
     }
 
+    // update every second
+    setInterval(displayTime, 1000)
 
-setInterval(displayTime, 1000)
+    function updateHour() {
+        let currentHour = dayjs().hour();
+        let timeBlocks = $(".time-block");
 
-function updateHour() {
-    let currentHour = dayjs().hour();
-    let timeBlocks = $(".time-block"); 
+        // if loop > used to cycle through time conditions for functionality and styling
+        timeBlocks.each(function () {
+            let blockHour = parseInt($(this).attr("id"));
 
-    timeBlocks.each(function() {
-        let blockHour = parseInt($(this).attr("id"));
+            if (currentHour > blockHour) {
+                $(this).removeClass("present future");
+                $(this).addClass("past");
+            } else if (currentHour === blockHour) {
+                $(this).removeClass("past future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past present");
+                $(this).addClass("future");
+            }
+        });
+    }
 
-        if (currentHour > blockHour) {
-            $(this).removeClass("present future"); 
-            $(this).addClass("past");
-        } else if (currentHour === blockHour) {
-            $(this).removeClass("past future"); 
-            $(this).addClass("present");
-        } else {
-            $(this).removeClass("past present"); 
-            $(this).addClass("future");
-        }
+    // Calling the function to update the hours
+    updateHour();
+
+    // assigning click function on saveBtn & setting items to local storage
+    $('.saveBtn').on('click', function () {
+        let textValue = $(this).siblings('.description').val();
+        let timeValue = $(this).parent().attr('id');
+        localStorage.setItem(timeValue, textValue);
     });
-}
 
-// Calling the function to update the hours
-updateHour();
+    // retrieving from local storage
+    $('.time-block').each(function () {
+        let hour = $(this).attr('id');
+        let storedValue = localStorage.getItem(hour);
+        $(this).find('.description').val(storedValue);
+        console.log(`Retrieved: ${hour} - ${storedValue}`);
+    });
 
-$('.saveBtn').on('click', function() {
-    let textValue = $(this).siblings('.description').val();
-    let timeValue = $(this).parent().attr('id');
-    localStorage.setItem(timeValue, textValue);
-    console.log(`Saved: ${timeValue} - ${textValue}`);
-});
-
-$('.time-block').each(function() {
-    let hour = $(this).attr('id');
-    let storedValue = localStorage.getItem(hour);
-    $(this).find('.description').val(storedValue);
-    console.log(`Retrieved: ${hour} - ${storedValue}`);
-});
-
-// const saveMessage = `Saved Event`
-// $(`.saveBtn`).on(`click`)
-// , function() {
-// $(`#eventMessage`).innerHTML = saveMessage;
-// }
-
-
-$(`#clearAllBtn`).on(`click`, function() {
-    localStorage.clear()
-    document.location.reload();
-})
+    // adding clear all functionality to the page to clear local storage
+    $(`#clearAllBtn`).on(`click`, function () {
+        localStorage.clear()
+        document.location.reload();
+    })
 
 });
 
